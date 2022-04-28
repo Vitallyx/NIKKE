da2bd8e569c2a6c532670b1478f2b3a6101057b8
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2022-04-29 01:09:07 +0200

    1

--- includes/personnages.php
+++ includes/personnages.php
@@ -21,7 +21,6 @@
                 </a>
             <?php
             }
-
             ?>
         </section>
     </article>
--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php
 require 'includes/header.php';
 
+$path =  'characters.json';
+
+$characters = file_get_contents($path);
+
+$characters = json_decode($characters, true);
+
+$page = $_GET['page'] ?? 'accueil';
+
 switch ($page) {
     case 'accueil':
         require 'includes/accueil.php';
         break;
+
+        // case $characters[every character]['character_name']:
+        //     require 'includes/templater.php';
+        //     break;
 
     case 'personnages':
         require 'includes/personnages.php';
@@ -13,6 +25,7 @@ switch ($page) {
     case 'guides':
         require 'includes/guides.php';
         break;
+
 
     case 'tier_list':
         require 'includes/tier_list.php';
