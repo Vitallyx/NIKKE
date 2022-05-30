fc8e344f8985e0c70732dcb2379b5d5ca539fad9
Bryan Muller <71022676+Vitallyx@users.noreply.github.com>
2022-05-30 09:13:42 +0200

    1

---
+++ assets/js/character.js
@@ -0,0 +1,27 @@
+function getQueryVariable(variable) {
+    let query = window.location.search.substring(1);
+    let vars = query.split("?");
+    for (let i = 0; i < vars.length; i++) {
+        let pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
+}
+
+let id = getQueryVariable("id")
+
+
+async function fetchData() {
+    const response = await fetch('./characters.json');
+    const data = await response.json();
+
+
+
+
+    const test = document.querySelector('.test')
+
+    test.innerHTML = "salut" + " " + data[id].character_name
+    console.log(data[id].character_name)
+
+}
+fetchData();
--- assets/js/characters.js
+++ assets/js/characters.js
@@ -3,12 +3,6 @@
 
 const main = document.getElementById('list-character');
 const information = document.querySelector('.information');
-const close = document.querySelector('.fa-times');
-
-
-const thumbnail = document.querySelector('.thumbnail');
-const char_title = document.querySelector('.char_title');
-const description = document.querySelector('.description')
 
 async function fetchData() {
     const response = await fetch('./characters.json');
@@ -16,27 +10,12 @@ async function fetchData() {
 
     data.map((val) => {
         const div = document.createElement("div");
-        div.innerHTML = "<img src='" + val.character_thumbnail + "'/><h1>" + val.character_name + "</h1>";
+        div.innerHTML = "<a href='character.html?id='" + val.character_id + "')><img src='" + val.character_thumbnail + "'/><h1>" + val.character_name + "</h1></a>";
         div.classList.add(val.character_rarity);
         main.appendChild(div);
 
-
-        div.addEventListener('click', (e) => {
-            console.log(val.character_name);
-            information.classList.toggle('show');
-
-            description.innerHTML = val.character_description;
-            thumbnail.src = val.character_thumbnail;
-
-            char_title.innerHTML = val.character_name;
-        })
     })
 
-}
 
-///////////////////////////////////
-// Open Characters 
-close.addEventListener('click', function() {
-    information.classList.toggle('show');
-})
+}
 fetchData();
