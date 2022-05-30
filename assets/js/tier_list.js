89b701badcd640fc13b4f163d5331255da872bdf
Bryan Muller <71022676+Vitallyx@users.noreply.github.com>
2022-05-30 09:48:47 +0200

    1

--- assets/js/tier_list.js
+++ assets/js/tier_list.js
@@ -1,11 +1,11 @@
 ///////////////////////////////////
 // Tier List
 
-const ssTier = document.getElementById('ssTier')
-const sTier = document.getElementById('sTier')
-const aTier = document.getElementById('aTier')
-const bTier = document.getElementById('bTier')
-const cTier = document.getElementById('cTier')
+const ssTier = document.getElementById('ssTier');
+const sTier = document.getElementById('sTier');
+const aTier = document.getElementById('aTier');
+const bTier = document.getElementById('bTier');
+const cTier = document.getElementById('cTier');
 
 async function fetchData() {
     const response = await fetch('./characters.json');
@@ -13,35 +13,40 @@ async function fetchData() {
 
     data.map((val) => {
         if (val.character_rank == 0) {
-            const ssTierChild = document.createElement("div");
-            ssTierChild.innerHTML = "<img src='" + val.character_thumbnail + "'/>"
-            ssTierChild.classList.add(val.character_rarity)
-            ssTier.appendChild(ssTierChild)
+            const ssTierChild = document.createElement("a");
+            ssTierChild.innerHTML = "<img src='" + val.character_thumbnail + "'/>";
+            ssTierChild.href = "character.html?id=" + val.character_id
+            ssTierChild.classList.add(val.character_rarity);
+            ssTier.appendChild(ssTierChild);
         }
         if (val.character_rank == 1) {
-            const sTierChild = document.createElement("div");
-            sTierChild.innerHTML = "<img src='" + val.character_thumbnail + "'/>"
-            sTierChild.classList.add(val.character_rarity)
-            sTier.appendChild(sTierChild)
+            const sTierChild = document.createElement("a");
+            sTierChild.innerHTML = "<img src='" + val.character_thumbnail + "'/>";
+            sTierChild.href = "character.html?id=" + val.character_id
+            sTierChild.classList.add(val.character_rarity);
+            sTier.appendChild(sTierChild);
         }
         if (val.character_rank == 2) {
-            const aTierChild = document.createElement("div");
-            aTierChild.innerHTML = "<img src='" + val.character_thumbnail + "'/>"
-            aTierChild.classList.add(val.character_rarity)
-            aTier.appendChild(aTierChild)
+            const aTierChild = document.createElement("a");
+            aTierChild.innerHTML = "<img src='" + val.character_thumbnail + "'/>";
+            aTierChild.href = "character.html?id=" + val.character_id
+            aTierChild.classList.add(val.character_rarity);
+            aTier.appendChild(aTierChild);
         }
         if (val.character_rank == 3) {
-            const bTierChild = document.createElement("div");
-            bTierChild.innerHTML = "<img src='" + val.character_thumbnail + "'/>"
-            bTierChild.classList.add(val.character_rarity)
-            bTier.appendChild(bTierChild)
+            const bTierChild = document.createElement("a");
+            bTierChild.innerHTML = "<img src='" + val.character_thumbnail + "'/>";
+            bTierChild.href = "character.html?id=" + val.character_id
+            bTierChild.classList.add(val.character_rarity);
+            bTier.appendChild(bTierChild);
         }
         if (val.character_rank == 4) {
-            const cTierChild = document.createElement("div");
-            cTierChild.innerHTML = "<img src='" + val.character_thumbnail + "'/>"
-            cTierChild.classList.add(val.character_rarity)
-            cTier.appendChild(cTierChild)
+            const cTierChild = document.createElement("a");
+            cTierChild.innerHTML = "<img src='" + val.character_thumbnail + "'/>";
+            cTierChild.href = "character.html?id=" + val.character_id
+            cTierChild.classList.add(val.character_rarity);
+            cTier.appendChild(cTierChild);
         }
     })
 }
-fetchData()
+fetchData();
