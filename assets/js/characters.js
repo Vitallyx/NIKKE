df2dde7f69a338bbc3b9aa3378d83b9fa6677490
Bryan Muller <71022676+Vitallyx@users.noreply.github.com>
2022-05-30 09:19:47 +0200

    1

--- assets/js/characters.js
+++ assets/js/characters.js
@@ -10,7 +10,7 @@ async function fetchData() {
 
     data.map((val) => {
         const div = document.createElement("div");
-        div.innerHTML = "<a href='character.html?id='" + val.character_id + "')><img src='" + val.character_thumbnail + "'/><h1>" + val.character_name + "</h1></a>";
+        div.innerHTML = "<a href='character.html?id=" + val.character_id + "')><img src='" + val.character_thumbnail + "'/><h1>" + val.character_name + "</h1></a>";
         div.classList.add(val.character_rarity);
         main.appendChild(div);
 
