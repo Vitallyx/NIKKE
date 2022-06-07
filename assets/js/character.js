398fdc8cb9cddfba92e0e9f6b245e7676f48e9df
Bryan Muller <71022676+Vitallyx@users.noreply.github.com>
2022-06-07 10:09:36 +0200

    1

--- assets/js/character.js
+++ assets/js/character.js
@@ -32,6 +32,8 @@ async function fetchData() {
     const faction_chr = document.querySelector('.faction_chr');
 
     const rarity = document.querySelector('.rarity');
+    const step = document.querySelector('.step');
+    const role = document.querySelector('.role');
     const name = document.querySelector('.name');
     const cv = document.querySelector('.cv');
     const story = document.querySelector('.story');
@@ -50,6 +52,20 @@ async function fetchData() {
     cv.innerHTML = "CV : " + db[id].character_cv;
     name.innerHTML = db[id].character_name;
     img_chr.src = db[id].character_images;
+
+    if (db[id].character_step == 1) {
+        step.src = "images/step1.webp"
+    } else if (db[id].character_step == 2) {
+        step.src = "images/step2.webp"
+    } else if (db[id].character_step == 3) {
+        step.src = "images/step3.webp"
+    }
+
+    if (db[id].character_role == 1) {
+        role.src = "images/defender.webp"
+    }
+
+
 
     if (db[id].character_rarity == 0) {
         rarity.src = "images/ssr.webp"
@@ -97,6 +113,20 @@ async function fetchData() {
     skill2_title.innerHTML = db[id].character_skill2_title;
     skill2_title.style.color = db[id].character_color;
     skill2_desc.innerHTML = db[id].character_skill2;
+
+    const ultimate = document.querySelector('.skill_ultimate');
+    ultimate.style.borderBottom = "5px solid" + db[id].character_color;
+
+    const ultimate_title = document.querySelector('.ultimate_title');
+    const ultimate_cd = document.querySelector('.ultimate_cd');
+    const ultimate_desc = document.querySelector('.ultimate_desc');
+
+    ultimate_cd.innerHTML = db[id].character_ultimate_cd;
+    ultimate_title.innerHTML = db[id].character_ultimate_title;
+    ultimate_title.style.color = db[id].character_color;
+    ultimate_desc.innerHTML = db[id].character_ultimate;
+
+
 
 }
 fetchData();
